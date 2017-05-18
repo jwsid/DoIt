4fe5fc5734ebf68981661d7e3392bfaecfe8ecc1
Justin W Siddons <jwsiddons@gmail.com>
2017-05-17 22:45:46 -0500

    App is working but no Core data ;(

---
+++ DoIt/CreateTaskViewController.swift
@@ -0,0 +1,41 @@
+//
+//  CreateTaskViewController.swift
+//  DoIt
+//
+//  Created by Justin W Siddons on 5/17/17.
+//  Copyright © 2017 Justin W Siddons. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var importantSwitch: UISwitch!
+    @IBOutlet weak var taskNameTextField: UITextField!
+    
+    var previousVC = TasksViewController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+        //Create a Task from the outlet information
+        
+        let task = Task()
+        task.name = taskNameTextField.text!
+        task.important = importantSwitch.isOn
+        
+        
+        //Add new task to array in previous view controller
+        
+        previousVC.tasks.append(task)
+        previousVC.tableView.reloadData()
+        navigationController!.popViewController(animated: true)
+        
+    }
+   
+
+}
---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Justin W Siddons on 5/17/17.
+//  Copyright © 2017 Justin W Siddons. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
